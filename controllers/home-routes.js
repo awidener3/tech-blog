281cf98ab08452806553237d323ce31dff549ce2
Alex Widener <awidener3@gmail.com>
2022-02-09 09:25:36 -0500

    began work on routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,20 +3,27 @@ const { Post, Comment, User } = require('../models');
 
 // get all posts for homepage
 router.get('/', async (req, res) => {
-  
+	try {
+		const postData = await Post.findAll();
+
+		// Serialize data
+		const posts = postData.map((post) => post.toJSON());
+
+		// Pass serialized data and session flag into template
+		res.render('all-posts', {
+			posts,
+			logged_in: req.session.logged_in,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
-// get single post
-router.get('/post/:id', async (req, res) => {
- 
-});
+// get single post by id
+router.get('/post/:id', async (req, res) => {});
 
-router.get('/login', (req, res) => {
+router.get('/login', (req, res) => {});
 
-});
-
-router.get('/signup', (req, res) => {
- 
-});
+router.get('/signup', (req, res) => {});
 
 module.exports = router;
